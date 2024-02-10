90842a4a41d39a7cb86cf2fa285d0e17c963a826
Rayan Obeid <rayan.obeid93@gmail.com>
2024-02-10 23:58:45 +0200

    Chat section

--- src/Pages/Settings/Settings.jsx
+++ src/Pages/Settings/Settings.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import './Settings.css'
+import SettingsList from '../../components/Settings/SettingsList/SettingsList'
+import { Outlet } from 'react-router-dom'
 
 const Settings = () => {
   return (
-    <div style={{color: 'white'}}>Settings</div>
+    <div className='settings'>
+        <SettingsList />
+        <Outlet />
+    </div>
   )
 }
 
