d66285fe6fe571142562d3273d331e4e4709aa0f
Batoul Hassan <batoulhassan264@gmail.com>
2024-02-19 01:18:48 +0300

    add popup files

--- src/components/Navbar/Nanbar.jsx
+++ src/components/Navbar/Nanbar.jsx
@@ -6,8 +6,21 @@ import img4 from "./../../assets/img/Notification-Icon.svg"
 import img5 from "./../../assets/img/search-normal.svg"
 import img6 from "./../../assets/img/UK.png"
 import img7 from "./../../assets/img/Arrow-Icon.svg"
+import { useState } from "react";
+import Language from '../Popup/Language/Language';
+import PopupSettings from "../Popup/PopupSettings/PopupSettings";
+import AllNotifications from "../Popup/NotificationPopup/AllNotifications/AllNotifications";
+import AllCurrencies from "../Popup/CurrencyPopup/AllCurrencies/AllCurrencies";
+
 const Nanbar = () => {
+
+    const [clickedLanguage, setClickedLanguage] = useState(false)
+    const [clickedSettings, setClickedSettings] = useState(false)
+    const [clickedNotificatin, setClickedNotificatin] = useState(false)
+    const [openCurrency, setOpenCurrency] = useState(false)
+
     return (
+      <>
         <nav className="AB-navbar">
             <div className="AB-menu-form">
                 <button className="AB-button-menu">
@@ -19,7 +32,7 @@ const Nanbar = () => {
                 </form>
             </div>
             <div className="AB-Bart2">
-                <button className="AB-nav-select">
+                <button className="AB-nav-select" onClick={() => setClickedLanguage(true)}>
                     <img src={img6} alt="" />
                     <p>English</p>
                     <img src={img7} alt="" />
@@ -27,14 +40,30 @@ const Nanbar = () => {
                 <button className="AB-nav-icon">
                     <img src={img2} alt="" />
                 </button>
-                <button className="AB-nav-icon">
+                <button className="AB-nav-icon" onClick={() => setClickedSettings(true)}>
                     <img src={img3} alt="" />
                 </button>
-                <button className="AB-nav-icon">
+                <button className="AB-nav-icon" onClick={() => setClickedNotificatin(true)}>
                     <img src={img4} alt="" />
                 </button>
             </div>
         </nav>
+        {
+            clickedLanguage && <Language setClicked={setClickedLanguage} />
+        }
+
+        {
+            clickedSettings && <PopupSettings  setClicked={setClickedSettings} setOpenCurrency={setOpenCurrency}/>
+        }
+
+        {
+            clickedNotificatin && <AllNotifications setClicked={setClickedNotificatin} />
+        }
+
+        {
+            openCurrency && <AllCurrencies setClicked={setOpenCurrency} />
+        }
+     </>
     )
 }
 
